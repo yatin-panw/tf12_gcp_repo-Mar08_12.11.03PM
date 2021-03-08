e1bb0f1d5da2932416aa032e75e9c0553086ae59
Giang Nguyen <ginguyen@paloaltonetworks.com>
2021-03-08 12:11:07 -0800

    Modified files:[main.tf]

--- gcp_compute_firewall/main.tf
+++ gcp_compute_firewall/main.tf
@@ -47,3 +47,5 @@ resource "google_compute_firewall" "default" {
 output "ip" {
  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
+
+# Modified [main.tf] on [2021-03-08 12:11:07.262562]
